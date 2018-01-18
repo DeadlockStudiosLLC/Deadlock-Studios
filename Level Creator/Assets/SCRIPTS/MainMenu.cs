8fa201d8293b906b82f9c73c4a160db4de8fe2d9
FrankGem <frank@deadlockstudios.com>
2018-01-18 14:40:30 -0500

    Adding Map Editor Project

---
+++ Level Creator/Assets/SCRIPTS/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class MainMenu : MonoBehaviour {
+    [SerializeField] MapEditorManager _mapEditorManager;
+
+    [SerializeField] TMPro.TMP_InputField _heightField;
+    [SerializeField] TMPro.TMP_InputField _widthField;
+
+    [SerializeField] TMPro.TMP_Dropdown _loadDropdown;
+
+    public void EnterEditorPressed() {
+        //if(_loadDropdown.)
+        _mapEditorManager.mapHeight = int.Parse(_heightField.text);
+        _mapEditorManager.mapWidth = int.Parse(_widthField.text);
+        _mapEditorManager.mapSize = _mapEditorManager.mapHeight * _mapEditorManager.mapWidth;
+    }
+
+    public void QuitApplication() {
+        Application.Quit();
+    }
+}
---
+++ Level Creator/Assets/SCRIPTS/MapEditorManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapEditorManager : MonoBehaviour {
+    int _mapSize;
+    public int mapSize { get { return _mapSize; } set { _mapSize = value; } }
+    int _mapHeight;
+    public int mapHeight { get { return _mapHeight; } set { _mapHeight = value; } }
+    int _mapWidth;
+    public int mapWidth { get { return _mapWidth; } set { _mapWidth = value; } }
+
+    string _mapName;
+    public string mapName { get { return _mapName; } set { _mapName = value; } }
+    string _savePath;
+    public string savePath { get { return _savePath; } set { _savePath = value; } }
+
+    private void Awake() {
+        DontDestroyOnLoad(this);
+    }
+}
